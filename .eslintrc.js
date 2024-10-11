21acee5d35468d3625e6cb741678a10bb693af06
JmarkMunda <jaymarkmunda26@gmail.com>
2024-10-11 16:37:46 -0700

    chore: modify eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,14 @@ module.exports = {
   extends: "@react-native",
   rules: {
     quotes: ["error", "double"],
+    "no-unused-vars": [
+      "error",
+      {vars: "all", args: "none", ignoreRestSiblings: false},
+    ],
+    indent: ["error", 2],
+    semi: ["error", "always"],
+    curly: "off",
+    "no-var": "error",
+    "@typescript-eslint/no-explicit-any": "error",
   },
 };
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,6 @@
-import {StyleSheet, TextInput} from 'react-native';
-import React from 'react';
-import {Props} from './types';
+import {StyleSheet, TextInput} from "react-native";
+import React from "react";
+import {Props} from "./types";
 
 const Input = ({value, onChangeText, placeholder, ...props}: Props) => {
   return (
@@ -19,7 +19,7 @@ export default Input;
 const styles = StyleSheet.create({
   input: {
     borderWidth: 1,
-    borderColor: '#cccc',
+    borderColor: "#cccc",
     borderRadius: 8,
     paddingVertical: 8,
     paddingHorizontal: 16,
