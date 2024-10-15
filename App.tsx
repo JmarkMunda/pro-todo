7365e64d10fa8de00487e1e41a5aaca0651d70a4
JmarkMunda <jaymarkmunda26@gmail.com>
2024-10-14 20:32:46 -0700

    feat: add splashscreen for the app

--- App.tsx
+++ App.tsx
@@ -4,11 +4,12 @@ import {NavigationContainer} from "@react-navigation/native";
 import {GestureHandlerRootView} from "react-native-gesture-handler";
 import FlashMessage from "react-native-flash-message";
 import MainNavigator from "./src/navigators/MainNavigator";
+import BootSplash from "react-native-bootsplash";
 
 function App(): React.JSX.Element {
   return (
     <GestureHandlerRootView>
-      <NavigationContainer>
+      <NavigationContainer onReady={() => BootSplash.hide({fade: true})}>
         <MainNavigator />
       </NavigationContainer>
       <FlashMessage position="top" floating />
